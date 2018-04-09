2696ae37c87785bbd654b5f13807127e3fc11614
linus.zhou <342174066@qq.com>
2018-04-09 14:29:59 +0800

    countdown

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,16 +4,16 @@
      <h1>{{ msg }}</h1>
     </section>
     <section id="option">
-     <el-button id="hold" type="primary">暂停</el-button>
-     <el-button id="end" type="primary">结束</el-button>
+     <el-button id="hold" type="primary" v-on:click="begin()">开始</el-button>
+     <el-button id="end" type="primary" v-on:click="reset()">重置</el-button>
     </section>
     <section>
-      <el-form :inline="true" class="demo-form-inline">
-        <el-form-item label="审批人">
-         <el-input placeholder="审批人"></el-input>
+      <el-form :inline="true" :model="time" :rules="rule" ref="time" class="demo-form-inline">
+        <el-form-item label="倒数几分钟啊" prop="minute">
+         <el-input v-model="time.minute" placeholder="几分钟呢"></el-input>
         </el-form-item>
         <el-form-item>
-        <el-button type="primary">开始计时</el-button>
+        <el-button type="primary" v-on:click="confirm('time')">确定</el-button>
         </el-form-item>
       </el-form>
     </section>
@@ -24,9 +24,92 @@
 export default {
   name: 'HelloWorld',
   data () {
+    var checkMinute = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('不能为空'))
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(Number(value))) {
+          callback(new Error('请输入整数值'))
+        } else {
+          callback()
+        }
+      }, 1000)
+    }
     return {
-      msg: '20:23:32',
-      time: 0
+      intervalId: null,
+      msg: '00:00:00',
+      time: {
+        minute: ''
+      },
+      secend: 0,
+      rule: {
+        minute: [
+          {validator: checkMinute, trigger: 'blur'}
+        ]
+      }
+    }
+  },
+  methods: {
+    confirm (time) {
+      this.$refs[time].validate((valid) => {
+        if (valid) {
+          this.setSecend()
+          this.printTime()
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    setSecend () {
+      this.secend = (this.time.minute * 60)
+    },
+    getTime (secend) {
+      var time = {
+        hours: '',
+        minute: '',
+        secend: ''
+      }
+
+      var hours = String(secend / 3600)
+      time.hours = (hours.split('.')[0])
+      secend = secend % 3600
+      var minute = String(secend / 60)
+      time.minute = (minute.split('.')[0])
+      time.secend = String(secend % 60)
+      if (time.secend.length === 1) {
+        time.secend = '0' + time.secend
+      }
+      if (time.hours.length === 1) {
+        time.hours = '0' + time.hours
+      }
+      if (time.minute.length === 1) {
+        time.minute = '0' + time.minute
+      }
+      return time
+    },
+    // 输出时间
+    printTime () {
+      var time = this.getTime(this.secend)
+      this.msg = time.hours + ':' + time.minute + ':' + time.secend
+    },
+    // 倒计时
+    countDown () {
+      this.printTime()
+      if (this.secend <= 0) {
+        window.clearInterval(this.intervalId)
+      }
+      this.secend -= 1
+    },
+    begin () {
+      this.intervalId = setInterval(() => {
+        this.countDown()
+      }, 1000)
+    },
+    reset () {
+      clearInterval(this.intervalId)
+      this.setSecend()
     }
   }
 }
@@ -35,12 +118,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #countdown {
-  margin-top: 200px;
+  margin-top: 250px;
 }
 #option {
-  margin-bottom: 20px;
+  margin-bottom: 50px;
 }
 h1 {
-  font-size: 100px;
+  font-size: 160px;
 }
 </style>
