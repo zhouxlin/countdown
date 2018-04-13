b9001bb059a0c80faede86c48bf36f27b97c3a64
linus.zhou <342174066@qq.com>
2018-04-13 17:28:53 +0800

    add color change

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>{{ clock }}</h1>
+    <h1 :class="{ red: isRed }">{{ clock }}</h1>
   </div>
 </template>
 
@@ -11,33 +11,39 @@ export default {
   },
   data () {
     return {
-      clock: '00:00:00'
+      clock: '00:00:00',
+      isRed: false
     }
   },
   methods: {
     show (secend) {
+      this.changeColor(secend)
       var time = {
         hours: '',
         minute: '',
         secend: ''
       }
-      console.log(secend)
       var hours = String(secend / 3600)
       time.hours = (hours.split('.')[0])
       secend = secend % 3600
       var minute = String(secend / 60)
       time.minute = (minute.split('.')[0])
       time.secend = String(secend % 60)
-      if (time.secend.length === 1) {
-        time.secend = '0' + time.secend
-      }
-      if (time.hours.length === 1) {
-        time.hours = '0' + time.hours
-      }
-      if (time.minute.length === 1) {
-        time.minute = '0' + time.minute
-      }
+      // 处理输出的格式
+      time.secend = '0' + time.secend
+      time.secend = time.secend.substr(-2, 2)
+      time.hours = '0' + time.hours
+      time.hours = time.hours.substr(-2, 2)
+      time.minute = '0' + time.minute
+      time.minute = time.minute.substr(-2, 2)
       this.clock = time.hours + ':' + time.minute + ':' + time.secend
+    },
+    changeColor (secend) {
+      if (secend < 10) {
+        this.isRed = true
+      } else {
+        this.isRed = false
+      }
     }
   }
 }
@@ -46,5 +52,8 @@ export default {
 <style scoped>
 h1 {
   font-size: 160px;
+}
+.red {
+  color: #C00D0D;
 }
 </style>
